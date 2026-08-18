4df9c5d5d7ba5b943b3d16e512fed577ee819e18
amir <amir.fayaziazad@gmail.com>
2026-08-18 10:57:41 +0330

    Add user repository contract

---
+++ LibraryManagement.Domain/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using LibraryManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagement.Domain.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        User Add(User newUser);
+
+        User? GetById(int id);
+        User? GetByUsername(string username);
+        bool ExistsByUsername(string username);
+        List<User> GetAll();
+        //List<Book> GetLoanBooksByUsername(string username); 
+
+        void Update(User updatedUser);
+
+        void Delete(int id);
+    }
+}
