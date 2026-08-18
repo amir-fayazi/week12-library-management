afe35382addfe4dffec22862aa2ea87de7d484c9
amir <amir.fayaziazad@gmail.com>
2026-08-18 20:06:33 +0330

    Add book loan repository contract

---
+++ LibraryManagement.Domain/Contracts/Repositories/Interfaces/IBookLoanRepository.cs
@@ -0,0 +1,20 @@
+﻿
+
+using LibraryManagement.Domain.Entities;
+
+namespace LibraryManagement.Domain.Contracts.Repositories.Interfaces
+{
+    public interface IBookLoanRepository
+    {
+        BookLoan Add(BookLoan bookLoan);
+
+        BookLoan? GetById(int id);
+        List<Book> GetBorrowedBooksByUserId(int userId);
+        User? GetBorrowerByBookId(int bookId);
+
+        bool IsAvailable(int bookId);
+
+        void Update(BookLoan updatedBookLoan);
+        void Delete(int id);
+    }
+}
