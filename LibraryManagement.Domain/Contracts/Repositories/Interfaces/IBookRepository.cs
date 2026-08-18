012ba56897ecf515cfe4945578910f8926c1cd9d
amir <amir.fayaziazad@gmail.com>
2026-08-18 18:59:15 +0330

    Add book repository contract

---
+++ LibraryManagement.Domain/Contracts/Repositories/Interfaces/IBookRepository.cs
@@ -0,0 +1,21 @@
+﻿using LibraryManagement.Domain.Entities;
+
+
+namespace LibraryManagement.Domain.Contracts.Repositories.Interfaces
+{
+    public interface IBookRepository
+    {
+        Book Add(Book newBook);
+
+
+        Book? GetById(int id);
+        bool ExistsByTitle(string title);
+        List<Book> GetAll();
+        List<Book> GetAllAvailable();
+        List<Book> GetAllBorrowed();
+
+        void Delete(int id);
+        void Update(Book updatedBook);
+        
+    }
+}
