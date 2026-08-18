23edbf92e1b264c663a3407ed4b0a885f8cc7e5c
amir <amir.fayaziazad@gmail.com>
2026-08-18 14:21:57 +0330

    Add EF Core database context

--- LibraryManagement.Infrastructure/Data/AppDbContext.cs
+++ LibraryManagement.Infrastructure/Data/AppDbContext.cs
@@ -1,9 +1,21 @@
 ﻿
 
+using LibraryManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace LibraryManagement.Infrastructure.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<BookLoan> BookLoans { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                 @"Server=.;Database=Week12-LibraryManagementDb;Integrated Security=True;TrustServerCertificate=True;");
+        }
     }
 }
