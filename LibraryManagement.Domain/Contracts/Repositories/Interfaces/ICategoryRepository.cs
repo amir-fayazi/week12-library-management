a488b588d2d5b987ca10469c4f7e0f40a348d1af
amir <amir.fayaziazad@gmail.com>
2026-08-19 10:14:02 +0330

    Add category repository contract

---
+++ LibraryManagement.Domain/Contracts/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿
+
+using LibraryManagement.Domain.Entities;
+
+namespace LibraryManagement.Domain.Contracts.Repositories.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Category Add(Category newCategory);
+
+        Category? GetById(int id);
+        bool ExistsByName(string name);
+
+        List<Category> GetAll();
+        List<Book> GetBooksByCategoryName(string categoryName);
+
+        void Update(Category updatedCategory);
+        void Delete(int id);
+
+
+    }
+}
