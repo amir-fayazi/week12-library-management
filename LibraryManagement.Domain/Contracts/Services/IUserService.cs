4d71738e1ed65917440775b64d643609e63b69d8
amir <amir.fayaziazad@gmail.com>
2026-08-19 16:20:54 +0330

    Update user service contract

--- LibraryManagement.Domain/Contracts/Services/IUserService.cs
+++ LibraryManagement.Domain/Contracts/Services/IUserService.cs
@@ -1,7 +1,16 @@
-﻿namespace LibraryManagement.Domain.Contracts.Services
+﻿using LibraryManagement.Domain.Entities;
+
+namespace LibraryManagement.Domain.Contracts.Services
 {
     public interface IUserService
     {
+        User? GetById(int userId);
+        User? GetByUsername(string username);
+        List<User> GetAll();
 
+        void ChangeUsername(int userId, string username);
+        void ChangePassword(int userId, string password);
+
+        void Delete(int id);
     }
 }
