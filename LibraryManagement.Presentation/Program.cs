abcd3d42c2ea11dc90bce413f0186cc5948ff32a
amir <amir.fayaziazad@gmail.com>
2026-08-21 09:24:35 +0330

    Add main console menu

--- LibraryManagement.Presentation/Program.cs
+++ LibraryManagement.Presentation/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine(DateOnly.FromDateTime(DateTime.Now));
+﻿
+while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("===== Library Management =====");
+    Console.WriteLine("1. Login");
+    Console.WriteLine("2. Register");
+    Console.WriteLine("3. Exit");
+    Console.Write("Select an option: ");
+
+    var input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "1":
+            Console.WriteLine("Login selected");
+            break;
+
+        case "2":
+            Console.WriteLine("Register selected");
+            break;
+
+        case "3":
+            return;
+
+        default:
+            Console.WriteLine("Invalid option");
+            break;
+    }
+
+    Console.WriteLine("Press any key...");
+    Console.ReadKey();
+}
