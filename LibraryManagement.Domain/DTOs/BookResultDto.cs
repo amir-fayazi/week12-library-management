a20a67ecd389c3079556914ab6b3dd0c91fd08a5
amir <amir.fayaziazad@gmail.com>
2026-09-01 11:59:44 +0330

    add bookResultDto

---
+++ LibraryManagement.Domain/DTOs/BookResultDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagement.Domain.DTOs
+{
+    public class BookResultDto
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string CatergoryName { get; set; }
+    }
+}
